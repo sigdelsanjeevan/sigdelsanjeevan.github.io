c98c2562216cfe3a9092d3a0e45b86e01a9c4fa8
SANJEEVAN\Sanjeevan <sanjeevan_sigdel@yahoo.com>
2021-03-13 08:20:49 -0600

    maze completed

---
+++ js/maze.js
@@ -0,0 +1,30 @@
+$(document).ready(pageLoad);
+
+function pageLoad() {
+	$("#start").click(startGame);
+	var gameStatus = true;
+}
+
+function startGame() {
+	$("#status").text("Game On! Best of Luck 🤞");
+	gameStatus = true;
+	$(".boundary").hover(endGame);
+	$("#maze").mouseleave(endGame);	
+	$(".boundary").removeClass("youlose");
+	$("#end").hover(gameWon);
+}
+
+function endGame() {
+	if (gameStatus) {
+		$(".boundary").addClass("youlose");
+		$("#status").text("Oh no! You lost 😭");
+		gameStatus = false;
+	}
+}
+
+function gameWon() {
+	if (gameStatus) {
+		$("#status").text("You won! Congratulations 🥳🎉");
+		gameStatus = false
+	}
+}
